93094a46206c32058edd705a84218dfef598a6a4
Sklenik <99402413+Sklenik@users.noreply.github.com>
2023-11-14 23:24:43 +0100

    added FIXMEs

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ if __name__ == "__main__":
     game.play()
 
 # TODO create jsonParser to simplify JSON file creation? (a program to create JSON files out of simple .txt, I think it might help with creating different creatures, items, etc.)
+
+#FIXME if backack is full, and item can be added (count can be increased) backpackfull funciton still occurs
+#FIXME magic fail occurs twice (whic means it does double damage = BAD)
