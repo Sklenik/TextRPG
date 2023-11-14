7d0522be5982e139aecd9e0c1e44fb1c425ff188
Sklenik <99402413+Sklenik@users.noreply.github.com>
2023-11-14 22:24:28 +0100

    resolved TODO

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from Library import game
 if __name__ == "__main__":
     game.play()
 
-# TODO implement the drop system on enemy death + randomize backack size and loot + implement creaturesV2 !
 # TODO create jsonParser to simplify JSON file creation? (a program to create JSON files out of simple .txt, I think it might help with creating different creatures, items, etc.)
