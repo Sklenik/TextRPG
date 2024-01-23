dde66d63dc329bc1fc4add71c9d13abcd160fc8b
Sklenik <99402413+Sklenik@users.noreply.github.com>
2024-01-23 21:54:31 +0100

    removed commented code

--- Library/handlers/__init__.py
+++ Library/handlers/__init__.py
@@ -1,8 +1,2 @@
-# from .messageHandlers import *
-# from .playerInput import *
-# from .enemyAI import *
-# from .playerAttack import *
-# from .magicHandlers import *
-
 from .. import utils, jsonHelper, entities, game
 from . import messageHandlers, enemyAI, playerInput, playerAttack, magicHandlers
