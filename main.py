4d5e7c8920dc687a18efe645ed9b8edb40b60566
Sklenik <99402413+Sklenik@users.noreply.github.com>
2024-01-23 23:25:50 +0100

    resolved FIXME

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
 # TODO create jsonParser to simplify JSON file creation? (a program to create JSON files out of simple .txt, I think it might help with creating different creatures, items, etc.)
 
 #FIXME if backack is full, and item can be added (count can be increased) backpackfull funciton still occurs
-#FIXME magic fail occurs twice (whic means it does double damage = BAD) -> only the damage occurs twice? Chicken fail occured once
 
 #FIXME
 #  missing console output space
