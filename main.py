2749c0a6cfd1141611d093759e76432a93f720ab
Sklenik <99402413+Sklenik@users.noreply.github.com>
2024-01-23 21:43:36 +0100

    added fixmes from testing

--- main.py
+++ main.py
@@ -6,4 +6,14 @@ if __name__ == "__main__":
 # TODO create jsonParser to simplify JSON file creation? (a program to create JSON files out of simple .txt, I think it might help with creating different creatures, items, etc.)
 
 #FIXME if backack is full, and item can be added (count can be increased) backpackfull funciton still occurs
-#FIXME magic fail occurs twice (whic means it does double damage = BAD)
+#FIXME magic fail occurs twice (whic means it does double damage = BAD) -> only the damage occurs twice? Chicken fail occured once
+
+#FIXME
+#  missing console output space
+#   - when playerinput receives unknown input
+#   - when player uses backack and it is empty
+#   - 
+    
+#FIXME
+#  incorect value
+#   - when item is consumed, the incorect qty is displayed. Why is the qty displayed in the first place?
