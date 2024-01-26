a4ca97ecc05899ea0f1f2b04cbf6e1f86ccc69d0
Sklenik <99402413+Sklenik@users.noreply.github.com>
2024-01-26 22:12:49 +0100

    resolved some TODOs and FIXMEs

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
 
 # TODO create jsonParser to simplify JSON file creation? (a program to create JSON files out of simple .txt, I think it might help with creating different creatures, items, etc.)
 
-#FIXME
-#  missing console output space
-#   - when player uses backack and it is empty
+# TODO make the pink and pointy color rare and special somehow
+# TODO make it possible for creatures to spawn with 0 hp
+
+#TODO Finish and merge tommorow !
