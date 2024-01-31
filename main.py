3e1c48a42cdb0fe7ee029609732421d9f433b5f1
Sklenik <99402413+Sklenik@users.noreply.github.com>
2024-01-31 18:13:50 +0100

    removed or resolved some TODOs

--- main.py
+++ main.py
@@ -7,7 +7,3 @@ if __name__ == "__main__":
 
 # TODO make the pink and pointy color rare and special somehow
 # TODO make it possible for creatures to spawn with 0 hp
-
-#TODO push the changes (internet didn't work yesterday)
-#TODO finish the documentation, add templates, create pullrequest and don't forget to add patchnotes!
-#TODO merge into master next time hopefuly !
